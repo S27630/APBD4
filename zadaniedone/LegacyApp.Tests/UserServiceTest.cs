362bcf87f12092ff035f0424858d7a620e0c7f13
Oliwia Ostrowska <s27630@pjwstk.edu.pl>
2024-04-03 22:50:57 +0200

    proba

---
+++ zadaniedone/LegacyApp.Tests/UserServiceTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using JetBrains.Annotations;
+using LegacyApp;
+using Xunit;
+
+namespace LegacyApp.Tests;
+
+[TestSubject(typeof(UserService))]
+public class UserServiceTest
+{
+    [Fact]
+    public void AddUser_Should_Return_False_When_AgeIsBelow21()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser("Joe", "Doe", "johndoe@gmail.com", DateTime.Parse("2008-03-21"), 1);
+        Assert.False(addResult);
+    }
+    [Fact]
+    public void AddUser_Should_Return_False_When_Name_is_Null()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser("", "Doe", "johndoe@gmail.com", DateTime.Parse("1993-03-21"), 1);
+        Assert.False(addResult);
+    }
+    [Fact]
+    public void AddUser_Should_Return_False_When_Email_is_not_corect()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser("Joe", "Doe", "johndoegmailcom", DateTime.Parse("1993-03-21"), 1);
+        Assert.False(addResult);
+    }
+    [Fact]
+    public void AddUser_Should_Throw_Exception_When_NotExisting()
+    {
+        var userService = new UserService();
+
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var addResult = userService.AddUser("Joe", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 7);
+        });
+    }
+    [Fact]
+    public void AddUser_Should_Return_True_When_All_Is_Corect()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser("Joe", "Doe", "johndoe@gmail.com", DateTime.Parse("1993-03-21"), 1);
+        Assert.True(addResult);
+    }
+    [Fact]
+    public void AddUser_WithVeryImportantClient_NoCreditLimit()
+    {
+        var userService = new UserService();
+        var client = new Client { Type = "VeryImportantClient" };
+        var user = new User(); 
+
+        userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+
+        Assert.False(user.HasCreditLimit);
+    }
+    [Fact]
+    public void AddUser_WithImportantClient_CreditLimitDoubled()
+    {
+        var userService = new UserService();
+        var client = new Client { Type = "ImportantClient" };
+        var user = new User();
+        userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 3);
+
+        Assert.Equal(0, user.CreditLimit); 
+    }
+
+    [Fact]
+    public void AddUser_WithNormalClient_CreditLimitSet()
+    {
+        var userService = new UserService();
+        var client = new Client { Type = "NormalClient" };
+        var user = new User(); 
+
+        userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+
+        Assert.False(user.HasCreditLimit);
+    }
+    [Fact]
+    public void AddUser_VeryImportantClient_NoCreditLimit()
+    {
+        var userService = new UserService();
+        var client = new Client { Type = "VeryImportantClient" };
+
+        var result = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 2);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void AddUser_CreditLimitBelowThreshold_ReturnsFalse()
+    {
+        var userService = new UserService();
+        var client = new Client { Type = "NormalClient" }; 
+        var result = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("2000-03-21"), 1);
+
+        Assert.True(result);
+    }
+}
---
+++ zadaniedone/LegacyApp/UserService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        { if (IsNameNotEmpty(firstName) || IsSecondNameNotEmpty(lastName))
+            {
+                return false;
+            }
+
+            if (IsEmailCorect(email))
+            {
+                return false;
+            }
+
+            var now = DateTime.Now;
+            int age = CalculateAge(dateOfBirth, now);
+            if (WhetherItWasHisBirthdayYet(dateOfBirth, now)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            if (IsClientVip(client))
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (IsClientNormal(client))
+            {
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+            }
+
+            if (WhatIsUserLimitOnCredit(user))
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private static bool WhatIsUserLimitOnCredit(User user)
+        {
+            return user.HasCreditLimit && user.CreditLimit < 500;
+        }
+
+        private static bool WhetherItWasHisBirthdayYet(DateTime dateOfBirth, DateTime now)
+        {
+            return now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day);
+        }
+
+        private static bool IsClientNormal(Client client)
+        {
+            return client.Type == "ImportantClient";
+        }
+
+        private static bool IsClientVip(Client client)
+        {
+            return client.Type == "VeryImportantClient";
+        }
+
+        private static int CalculateAge(DateTime dateOfBirth, DateTime now)
+        {
+            return now.Year - dateOfBirth.Year;
+        }
+
+        private static bool IsEmailCorect(string email)
+        {
+            return !email.Contains("@") && !email.Contains(".");
+        }
+
+        private static bool IsSecondNameNotEmpty(string lastName)
+        {
+            return string.IsNullOrEmpty(lastName);
+        }
+
+        private static bool IsNameNotEmpty(string firstName)
+        {
+            return string.IsNullOrEmpty(firstName);
+        }
+    }
+}
